e02475562f61cf57ead3635ce0665e9ff54dd9c5
Hovhannes <hov.ghevondyan1994@gmail.com>
2022-07-29 15:21:37 +0400

    fix form

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".input-wrapper .input").keyup(function () {
+  $(".input-wrapper .input, .input-wrapper .textarea").keyup(function () {
     if ($(this).val() !== "") {
       $(this).addClass("focused");
     } else {
