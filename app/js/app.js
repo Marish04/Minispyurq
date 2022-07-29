2c6bc62f922793c1fed014d5149c4ed16a080e50
Hovhannes <hov.ghevondyan1994@gmail.com>
2022-07-29 17:03:21 +0400

    add dropdown

--- app/js/app.js
+++ app/js/app.js
@@ -6,4 +6,35 @@ $(document).ready(function () {
       $(this).removeClass("focused");
     }
   });
+
+  // checkout modal dropdown
+  function openDropdown() {
+    window.$(".dropdown").addClass("show");
+  }
+  function closeDropdown() {
+    window.$(".dropdown").removeClass("show");
+  }
+
+  // open/close modal
+  window.$(".dropdown").click(() => {
+    if (window.$(".dropdown").hasClass("show")) {
+      closeDropdown();
+    } else {
+      openDropdown();
+    }
+  });
+
+  // close dropdown clicking outside
+  window.$(document).on("click", function (e) {
+    if (!(window.$(e.target).closest(".dropdown").length > 0)) {
+      closeDropdown();
+    }
+  });
+
+  // set active item
+  window.$(".dropdown-item-li").click(function () {
+    const currentValue = window.$(".active-item");
+    // currentValue.html(window.$(this).html());
+    currentValue.text(window.$(this).text());
+  });
 });
